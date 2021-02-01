9e9fe9a552760671ab4db1286e3f2134caf490c2
mohammadalshuwaiee <shuwaiee1993@gmail.com>
2021-02-01 19:04:06 +0300

    Variables Task

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Mshary";
+
+let age = 25;
+
+let hoppy = "Meditation";
+
+        
+
+console.log(` Mr./Mrs. ${name}, in ${age + 33} years old. By then you'll have decades of ${hoppy} practice! `)
